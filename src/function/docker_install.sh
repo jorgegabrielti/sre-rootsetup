9d9eee974274393c0862e2a06e3e3e11deb0d092
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-03-23 17:11:50 -0300

    [update]: improvements.

--- src/function/docker_install.sh
+++ src/function/docker_install.sh
@@ -5,11 +5,13 @@ docker_install () {
   docker --version > /dev/null 2>&1 
   
   DOCKER_INSTALL_STATUS_CHECK="$?"
+
   if [[ ${DOCKER_INSTALL_STATUS_CHECK} == "0" ]]; then
     echo -e "\t\e[40;33;1m[Docker already installed]\e[m: \e[32;1m$(docker --version)\e[m\n"
   else 
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
     docker --version
+    sudo rm -f get-docker.sh
   fi 
 }
