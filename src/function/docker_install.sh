bedd80055bb3eda0ed3904a5aecebfcf566e6181
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-03-23 16:52:07 -0300

    [add]: function docker_install.sh.

---
+++ src/function/docker_install.sh
@@ -0,0 +1,15 @@
+# Test: [OK]
+docker_install () { 
+  echo -e "\e[40;32;1m[TASK]: docker_install\e[m\n"
+  
+  docker --version > /dev/null 2>&1 
+  
+  DOCKER_INSTALL_STATUS_CHECK="$?"
+  if [[ ${DOCKER_INSTALL_STATUS_CHECK} == "0" ]]; then
+    echo -e "\t\e[40;33;1m[Docker already installed]\e[m: \e[32;1m$(docker --version)\e[m\n"
+  else 
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    docker --version
+  fi 
+}
