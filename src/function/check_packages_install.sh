f3dfb38c33f4fe3adb36da69751207713e33caf3
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-03-24 15:26:42 -0300

    [add]: check_packages_install.sh.

---
+++ src/function/check_packages_install.sh
@@ -0,0 +1,14 @@
+# Test: [OK]
+check_packages_install () { 
+  echo -e "### Tools installed ###\n"
+  echo -e "Python    =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Docker    =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Kiind     =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Kubectl   =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Istio     =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Ansible   =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Vagrant   =======> \e[32;5;1m[OK]\e[m"
+  echo -e "Terraform =======> \e[32;5;1m[OK]\e[m"
+  echo -e "aws       =======> \e[32;5;1m[OK]\e[m"
+  echo -e "gcloud    =======> \e[32;5;1m[OK]\e[m"
+}
