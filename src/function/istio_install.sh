bb839a0211e9f2fdf2cd229bd55cf707ac1449b1
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-03-24 11:16:08 -0300

    [update] istio_install.sh.

--- src/function/istio_install.sh
+++ src/function/istio_install.sh
@@ -2,20 +2,17 @@
 istio_install () { 
   echo -e "\e[40;32;1m[TASK]: istio_install\e[m\n"
   
-  istioctl --version > /dev/null 2>&1 
+  istioctl version > /dev/null 2>&1
   
   ISTIO_INSTALL_STATUS_CHECK="$?"
 
   if [[ ${ISTIO_INSTALL_STATUS_CHECK} == "0" ]]; then
-    echo -e "\t\e[40;33;1m[Istio already installed]\e[m: \e[32;1m$(isctioctl --version)\e[m\n"
+    echo -e "\t\e[40;33;1m[Istio already installed]\e[m: \e[32;1m$(istioctl version 2> /dev/null)\e[m\n"
   else 
     curl -L https://istio.io/downloadIstio | sh -
     cd istio-*
-    
-    istioctl install \ 
-      --set profile=default -y \
-      --set meshConfig.accessLogFile=/dev/stdout
-    
-    istioctl --version
+    sudo cp -v ./bin/istioctl "$(echo ${PATH} | grep -Fo '/usr/bin')/istioctl"
+    istioctl version 2> /dev/null
+    rm -rf istio-*
   fi 
 }
