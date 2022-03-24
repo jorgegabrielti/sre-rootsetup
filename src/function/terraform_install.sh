23710a65e41541844a8a4f240b7bb46a956fd59a
jorgegabrielti <jorgegabriel.ti@gmail.com>
2022-03-23 21:26:04 -0300

    [add]: terraform_install.sh.

---
+++ src/function/terraform_install.sh
@@ -0,0 +1,20 @@
+# Test: [OK]
+terraform_install () { 
+  echo -e "\e[40;32;1m[TASK]: terraform_install\e[m\n"
+  
+  terraform -v > /dev/null 2>&1 
+  
+  TERRAFORM_INSTALL_STATUS_CHECK="$?"
+
+  if [[ ${TERRAFORM_INSTALL_STATUS_CHECK} == "0" ]]; then
+    echo -e "\t\e[40;33;1m[Terraform already installed]\e[m: \e[32;1m$(terraform -v)\e[m\n"
+  else 
+    curl -fsSL https://apt.releases.hashicorp.com/gpg \
+      | sudo apt-key add -
+    sudo apt-add-repository \
+      "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update -y \
+      && sudo apt-get install -y terraform
+    terraform -v
+  fi 
+}
